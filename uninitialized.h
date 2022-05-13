8b9adbc0d8b795e112d157afff4bee5a3042db20
ahuai974 <44867669+ahuai974@users.noreply.github.com>
2022-05-13 16:54:48 +0800

    mySTL

---
+++ uninitialized.h
@@ -0,0 +1,208 @@
+#ifndef PORJECT1_UNINITIALIZED_H
+#define PORJECT1_UNINITIALIZED_H
+//trivially copy assignable 能否简单的复制分配
+//这里的算法与algobase中大同小异，如果对象能简单的复制分配就调用algobase中的算法，否则使用构造析构的方法实现。
+//这里的copy与algobase中的copy不同，如果是false type利用开辟空间的方式复制，如果是true type则是algobase。
+namespace mySTL
+{
+	//*****************************************************************************************
+	// uninitialized_copy
+	// 把 [first, last) 上的内容复制到以 result 为起始处的空间，返回复制结束的位置
+	template <class InputIter, class ForwardIter>
+	ForwardIter
+		unchecked_uninit_copy(InputIter first, InputIter last, ForwardIter result, std::true_type)
+	{
+		return mySTL::copy(first, last, result);
+	}
+	//出现异常则直接析构
+	template <class InputIter, class ForwardIter>
+	ForwardIter
+		unchecked_uninit_copy(InputIter first, InputIter last, ForwardIter result, std::false_type)
+	{
+		auto cur = result;
+		try
+		{
+			for (; first != last; ++first, ++cur)
+			{
+				mySTL::construct(&*cur, *first);
+			}
+		}
+		catch (...)
+		{
+			for (; result != cur; ++result)
+				mySTL::destroy(&*result);
+		}
+		return cur;
+	}
+	// uninitialized_copy_n
+	// 把 [first, first + n) 上的内容复制到以 result 为起始处的空间，返回复制结束的位置
+
+	template <class InputIter, class Size, class ForwardIter>
+	ForwardIter
+		unchecked_uninit_copy_n(InputIter first, Size n, ForwardIter result, std::true_type)
+	{
+		return mySTL::copy_n(first, n, result).second;
+	}
+	template <class InputIter, class Size, class ForwardIter>
+	ForwardIter uninitialized_copy_n(InputIter first, Size n, ForwardIter result)
+	{
+		return mySTL::unchecked_uninit_copy_n(first, n, result,
+			std::is_trivially_copy_assignable<
+			typename iterator_traits<InputIter>::
+			value_type>{});
+	}
+	// uninitialized_fill
+	// 在 [first, last) 区间内填充元素值
+
+	template <class ForwardIter, class T>
+	void
+		unchecked_uninit_fill(ForwardIter first, ForwardIter last, const T& value, std::true_type)
+	{
+		mySTL::fill(first, last, value);
+	}
+
+	template <class ForwardIter, class T>
+	void
+		unchecked_uninit_fill(ForwardIter first, ForwardIter last, const T& value, std::false_type)
+	{
+		auto cur = first;
+		try
+		{
+			for (; cur != last; ++cur)
+			{
+				mySTL::construct(&*cur, value);
+			}
+		}
+		catch (...)
+		{
+			for (; first != cur; ++first)
+				mySTL::destroy(&*first);
+		}
+	}
+	template <class ForwardIter, class T>
+	void  uninitialized_fill(ForwardIter first, ForwardIter last, const T& value)
+	{
+		mystl::unchecked_uninit_fill(first, last, value,
+			std::is_trivially_copy_assignable<
+			typename iterator_traits<ForwardIter>::
+			value_type>{});
+	}
+	//*****************************************************************************************
+	// uninitialized_fill_n
+	// 从 first 位置开始，填充 n 个元素值，返回填充结束的位置
+
+	template <class ForwardIter, class Size, class T>
+	ForwardIter
+		unchecked_uninit_fill_n(ForwardIter first, Size n, const T& value, std::true_type)
+	{
+		return mySTL::fill_n(first, n, value);
+	}
+	template <class ForwardIter, class Size, class T>
+	ForwardIter
+		unchecked_uninit_fill_n(ForwardIter first, Size n, const T& value, std::false_type)
+	{
+		auto cur = first;
+		try
+		{
+			for (; n > 0; --n, ++cur)
+			{
+				mySTL::construct(&*cur, value);
+			}
+		}
+		catch (...)
+		{
+			for (; first != cur; ++first)
+				mySTL::destroy(&*first);
+		}
+		return cur;
+	}
+	template <class ForwardIter, class Size, class T>
+	ForwardIter uninitialized_fill_n(ForwardIter first, Size n, const T& value)
+	{
+		return mySTL::unchecked_uninit_fill_n(first, n, value,
+			std::is_trivially_copy_assignable<
+			typename iterator_traits<ForwardIter>::
+			value_type>{});
+	}
+	//*****************************************************************************************
+	// uninitialized_move
+	// 把[first, last)上的内容移动到以 result 为起始处的空间，返回移动结束的位置
+
+	template <class InputIter, class ForwardIter>
+	ForwardIter
+		unchecked_uninit_move(InputIter first, InputIter last, ForwardIter result, std::true_type)
+	{
+		return mySTL::move(first, last, result);
+	}
+	template <class InputIter, class ForwardIter>
+	ForwardIter
+		unchecked_uninit_move(InputIter first, InputIter last, ForwardIter result, std::false_type)
+	{
+		ForwardIter cur = result;
+		try
+		{
+			for (; first != last; ++first, ++cur)
+			{
+				mySTL::construct(&*cur, mystl::move(*first));
+			}
+		}
+		catch (...)
+		{
+			mySTL::destroy(result, cur);
+		}
+		return cur;
+	}
+	template <class InputIter, class ForwardIter>
+	ForwardIter uninitialized_move(InputIter first, InputIter last, ForwardIter result)
+	{
+		return mySTL::unchecked_uninit_move(first, last, result,
+			std::is_trivially_move_assignable<
+			typename iterator_traits<InputIter>::
+			value_type>{});
+	}
+	//*****************************************************************************************
+	// uninitialized_move_n
+	// 把[first, first + n)上的内容移动到以 result 为起始处的空间，返回移动结束的位置
+
+	template <class InputIter, class Size, class ForwardIter>
+	ForwardIter
+		unchecked_uninit_move_n(InputIter first, Size n, ForwardIter result, std::true_type)
+	{
+		return mySTL::move(first, first + n, result);
+	}
+	template <class InputIter, class Size, class ForwardIter>
+	ForwardIter
+		unchecked_uninit_move_n(InputIter first, Size n, ForwardIter result, std::false_type)
+	{
+		auto cur = result;
+		try
+		{
+			for (; n > 0; --n, ++first, ++cur)
+			{
+				mySTL::construct(&*cur, mystl::move(*first));
+			}
+		}
+		catch (...)
+		{
+			for (; result != cur; ++result)
+				mySTL::destroy(&*result);
+			throw;
+		}
+		return cur;
+
+	}
+	template <class InputIter, class Size, class ForwardIter>
+	ForwardIter uninitialized_move_n(InputIter first, Size n, ForwardIter result)
+	{
+		return mystl::unchecked_uninit_move_n(first, n, result,
+			std::is_trivially_move_assignable<
+			typename iterator_traits<InputIter>::
+			value_type>{});
+	}
+
+}
+
+
+
+
+#endif // !PORJECT1_UNINITIALIZED_H
